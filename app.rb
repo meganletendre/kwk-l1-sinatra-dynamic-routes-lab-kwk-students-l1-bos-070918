9460425459b3db9234d1bfbfd01f1fb1e29bc4d6
meganletendre <megan.letendre@comcast.net>
2018-07-18 13:29:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
     for i in 1..@num
      @all_phrases+="#{@phrases}\n"
     end
-    return @allphrases
+    return @all_phrases
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
