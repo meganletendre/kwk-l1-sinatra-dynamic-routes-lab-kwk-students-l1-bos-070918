cb34c2d753fac20d306787f7a12839c894c35af0
meganletendre <megan.letendre@comcast.net>
2018-07-18 13:02:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,8 +16,13 @@ class App < Sinatra::Base
     "The square is #{@answer}."
   end 
   
-  get '/say/:number/:phrase' do 
-  end 
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrases = params[:phrase]
+    @all_phrases = ""
+    for i in 1..@num
+     @all_phrases+="#{@phrases}\n"
+    end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word_1 = params[:word1]
